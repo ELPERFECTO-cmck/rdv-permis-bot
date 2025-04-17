880ad4fcf9c102815415d320edf6eea98b8927a6
ELPERFECTO-cmck <emericsainthubert26@gmail.com>
2025-04-17 20:19:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import os
 import time
 
 print("🟢 Bot (Firefox) démarré...")
 
-# ✅ Chemin de Firefox (version ESR installée sur Debian)
-firefox_path = "/usr/bin/firefox"
-
-# Vérifie si Firefox est bien là
-if not os.path.exists(firefox_path):
-    print("❌ Firefox introuvable à l'emplacement prévu.")
-    exit(1)
-
 options = Options()
-options.binary_location = firefox_path
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
@@ -28,6 +18,7 @@ try:
     print("✅ Terminé.")
 except Exception as e:
     print("❌ Erreur :", e)
+
 
 
 
